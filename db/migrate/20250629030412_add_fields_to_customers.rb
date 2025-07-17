e760675f97b6bdf99a2f32cd03f21fdc57224be4
venu bhat <venubha147@gmail.com>
2025-07-17 08:22:25 +0530

    Initial commit

--- db/migrate/20250629030412_add_fields_to_customers.rb
+++
@@ -1,9 +0,0 @@
-class AddFieldsToCustomers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :customers, :email, :string
-    add_column :customers, :gst_number, :string
-    add_column :customers, :pan_number, :string
-    add_column :customers, :member_id, :string
-    add_column :customers, :shipping_address, :string
-  end
-end
