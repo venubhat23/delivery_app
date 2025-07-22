4d676bd1e69bb898ae925dc7107c8b2dac78a242
JogendraDev30 <157616093+JogendraDev30@users.noreply.github.com>
2025-07-22 08:27:37 +0530

    Update development.rb to host ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "gnu-modern-totally.ngrok-free.app"
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
