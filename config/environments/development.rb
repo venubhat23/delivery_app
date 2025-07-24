1d73de7d6f101c5c321d04695616fca30fbe9818
venu bhat <venubha147@gmail.com>
2025-07-24 23:56:14 +0530

    test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,34 +7,8 @@ Rails.application.configure do
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
-  # Force HTTPS so it matches the ngrok URL
-  config.force_ssl = false
-  config.assume_ssl = false
-
-  # Ensure default_url_options use the ngrok HTTPS host
-  config.action_mailer.default_url_options = {
-    host: 'gnu-modern-totally.ngrok-free.app',
-    protocol: 'https'
-  }
-
-  config.action_controller.default_url_options = {
-    host: 'gnu-modern-totally.ngrok-free.app',
-    protocol: 'https'
-  }
-
-  # config.action_dispatch.default_headers = {
-  #   'X-Forwarded-Proto' => 'https'
-  # }
-
   # Do not eager load code on boot.
   config.eager_load = false
-config.assets.compile = false
-
-# This should be TRUE
-config.assets.digest = true
-
-# Ensure static files are served
-config.public_file_server.enabled = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
