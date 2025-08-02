512c6a130f84beeb7477ad843db0a1e2e8e48a91
venu bhat <venubha147@gmail.com>
2025-08-02 16:51:59 +0530

    test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -89,6 +89,8 @@ class Invoice < ApplicationRecord
   def public_url(host: nil, port: nil)
     url_options = { token: share_token }
     
+
+      url_options[:host] = "http://gnu-modern-totally.ngrok-free.app"
     
     
     Rails.application.routes.url_helpers.public_invoice_url(url_options)
