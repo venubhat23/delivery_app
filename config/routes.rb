010a7f626d05dd35d5f86381616d01666b5defc8
venu bhat <venubha147@gmail.com>
2025-10-01 09:08:39 +0530

    test

--- config/routes.rb
+++ config/routes.rb
@@ -307,6 +307,7 @@ Rails.application.routes.draw do
       get :monthly_preview
       get :generate_monthly_for_all
       post :generate_monthly_for_all
+      post :process_invoice_batch
       get :search_suggestions
       get :export_for_whatsapp
       post :generate_and_send_whatsapp
